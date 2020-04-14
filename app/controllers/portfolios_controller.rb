3a74723f5772db75d0b56fc7085bd297d5428b0a
Daniel Bekesi <bekesi.daniel@hotmail.hu>
2020-04-14 11:56:49 +0200

    Implement portfolio delete.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -37,8 +37,19 @@ class PortfoliosController < ApplicationController
 
     def show
        @portfolio_item = Portfolio.find(params[:id])
-       
     end
 
+    def destroy
+      # Look up portfolio
+       @portfolio_item = Portfolio.find(params[:id])
+
+       # Destroy, delete record
+       @portfolio_item.destroy
+
+      # Redirect
+       respond_to do |format|
+        format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully destroyed.' }
+      end
+    end
 
 end
