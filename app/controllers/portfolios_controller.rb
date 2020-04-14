70c796d4a74c4637173335288001b8c0919263d7
Daniel Bekesi <bekesi.daniel@hotmail.hu>
2020-04-14 11:35:01 +0200

    Implemented Show functionality.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -35,5 +35,10 @@ class PortfoliosController < ApplicationController
        @portfolio_item = Portfolio.find(params[:id])
     end
 
+    def show
+       @portfolio_item = Portfolio.find(params[:id])
+       
+    end
+
 
 end
