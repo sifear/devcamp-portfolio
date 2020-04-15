41834f04f048c09df8c41977ff97d864621e80ec
Daniel Bekesi <bekesi.daniel@hotmail.hu>
2020-04-15 10:59:57 +0200

    Concern for managing image data.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_064230) do
+ActiveRecord::Schema.define(version: 2020_04_15_084508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2020_04_15_064230) do
     t.integer "percent_utilised"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "badge"
   end
 
   create_table "topics", force: :cascade do |t|
