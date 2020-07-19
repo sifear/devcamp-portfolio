2fd0ad4dec37a47563ab293532acae62c7a6d8fb
Daniel Bekesi <bekesi.daniel@hotmail.hu>
2020-07-19 19:55:22 +0200

    Controller based functionalities.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -13,11 +13,11 @@ class PortfoliosController < ApplicationController
     end
 
     def create
-      @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+      @portfolio_item = Portfolio.new(portfolio_params)
 
       respond_to do |format|
         if @portfolio_item.save
-          format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.s' }
+          format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
         else
           format.html { render :new }
         end
@@ -28,7 +28,7 @@ class PortfoliosController < ApplicationController
       @portfolio_item = Portfolio.find(params[:id])
 
       respond_to do |format|
-        if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        if @portfolio_item.update(portfolio_params)
           format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
         else
           format.html { render :edit }
@@ -56,6 +56,15 @@ class PortfoliosController < ApplicationController
        respond_to do |format|
         format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully destroyed.' }
       end
+    end
+
+    private
+
+    def portfolio_params
+        params.require(:portfolio).permit(:title, 
+                                          :subtitle, 
+                                          :body, 
+                                          technologies_attributes: [:name])
     end
 
 end
